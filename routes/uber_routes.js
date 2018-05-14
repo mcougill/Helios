c720d30dee18a583eb5948c0a9eb3f9fba74d8ec
Molly Cougill <molly.cougill@gmail.com>
2018-05-13 20:31:23 -0500

    remove merge conflict arrows

--- routes/uber_routes.js
+++ routes/uber_routes.js
@@ -1,5 +1,6 @@
 const Uber = require("node-uber");
-<<<<<<< HEAD
+const express = require('express');
+const app = express();
 
 module.exports = function (app) {
 
@@ -31,70 +32,15 @@ module.exports = function (app) {
 
                 // redirect the user back to your actual app
                 response.redirect('/web/index.html');
-=======
-const app = express();
-
-//initialize Uber object
-const uber = new Uber({
-    client_id: 'process.env.client_id',
-    client_secret: 'process.env.client_secret',
-    server_token: 'process.env.server_token',
-    redirect_uri: '',
-    name: 'Student Project',
-    language: 'en_US',
-});
-
-
-//redirect user to authorization URL
-app.get('/api/login', function (request, response) {
-    var url = uber.getAuthorizeUrl(['history', 'profile', 'request', 'places', 'all_trips']);
-    //redirect to user login to approve access to Uber account 
-    response.redirect(url);
-});
-
-//receive redirect and conver authorization code into OAuth access token
-app.get('/api/callback', function (request, response) {
-    uber.authorizationAsync({ authorization_code: request.query.code })
-        .spread(function (access_token, refresh_token, authorizedScopes, tokenExpiration) {
-            // store the user id and associated access_token, refresh_token, scopes and token expiration date
-            console.log('New access_token retrieved: ' + access_token);
-            console.log('... token allows access to scopes: ' + authorizedScopes);
-            console.log('... token is valid until: ' + tokenExpiration);
-            console.log('... after token expiration, re-authorize using refresh_token: ' + refresh_token);
-
-            // redirect the user back to your actual app
-            response.redirect('../public.index.html');
-        })
-        .error(function (err) {
-            console.error(err);
-        });
-});
-
-//make HTTP requests to available resources
-app.get('/api/price', function (request, response) {
-
-    // extract the query from the request URL
-    const query = url.parse(request.url, true).query;
-    const start_latitude = query.lat;
-    const start_longitude = query.lng;
-    const end_latitude;
-    const end_longitude;
-
-    // if no query params sent, respond with Bad Request
-    if (!query || !start_latitude || !start_longitude) {
-        response.sendStatus(400);
-    } else {
-        uber.estimates.getPriceForRouteAsync(start_latitude, start_longitude, end_latitude, end_longitude)
-            .then(function (res) {
-                console.log(res);
->>>>>>> 6a456cd0c846c35dc954f007dfa1aa643e1cf6c0
             })
             .error(function (err) {
                 console.error(err);
             });
-<<<<<<< HEAD
     });
 
+
+
+    
     //make HTTP requests to available resources
     app.get('/api/price', function (request, response) {
 
@@ -115,11 +61,7 @@ app.get('/api/price', function (request, response) {
                 });
         }
     });
+
 };
-=======
-    }
-});
 
-module.exports = uber;
-
->>>>>>> 6a456cd0c846c35dc954f007dfa1aa643e1cf6c0
+   
