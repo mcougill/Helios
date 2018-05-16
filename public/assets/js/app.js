91955f58c5435a409fb6940a5995fa5595882f88
Vizena2005 <vizena2005@gmail.com>
2018-05-16 12:34:07 -0500

    restarting the page lorem ipsum and basic layout done

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,9 +1,3 @@
 $(document).ready(function() {
-  //   TOGGLES THE HAMBURGER MENU
-  $("#toggle").click(function() {
-    $(this).toggleClass("on");
-    $("#resize").toggleClass("active");
-  });
-  //
   // Closing tag for document.ready
 });
