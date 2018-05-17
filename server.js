61aed337fa1bf69daf11df63acdf205d6554376c
Chris Jones <chrisjones0517@gmail.com>
2018-05-17 09:51:28 -0500

    minor changes

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use('/auth', authRoutes);
 require('./routes/uber_routes.js')(app);
 // require('./routes/lyft_routes.js')(app);
 require('./routes/html_routes.js')(app);
-require('./routes/maps_routes.js')(app);
+// require('./routes/maps_routes.js')(app);
 
 const PORT = process.env.PORT || 3000;
 
