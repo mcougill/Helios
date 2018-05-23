50423d45b3246a4021cf54b2007b62a6489d0754
Chris Jones <chrisjones0517@gmail.com>
2018-05-23 15:52:43 -0500

    added streaming feature to background video

--- routes/html_routes.js
+++ routes/html_routes.js
@@ -88,6 +88,38 @@ module.exports = function (app) {
 
     });
 
+    app.get('/carsBG.mov', function (req, res) {
+
+        const path = __dirname + '/carsBG.mov';
+        const stat = fs.statSync(path);
+        const fileSize = stat.size;
+        const range = req.headers.range;
+        if (range) {
+            const parts = range.replace(/bytes=/, "").split("-");
+            const start = parseInt(parts[0], 10);
+            const end = parts[1]
+                ? parseInt(parts[1], 10)
+                : fileSize - 1;
+            const chunksize = (end - start) + 1;
+            const file = fs.createReadStream(path, { start, end });
+            const head = {
+                'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+                'Accept-Ranges': 'bytes',
+                'Content-Length': chunksize,
+                'Content-Type': 'video/mp4',
+            };
+            res.writeHead(206, head);
+            file.pipe(res);
+        } else {
+            const head = {
+                'Content-Length': fileSize,
+                'Content-Type': 'video/mp4',
+            }
+            res.writeHead(200, head);
+            fs.createReadStream(path).pipe(res);
+        }
+    });
+
     app.post('/register', function (req, res) {
         // users submitted info will be validated and queried against the database for duplicates, then upon success will be redirected to login page, otherwise return an error to the user
 
